8f72c260ae7c6dfa26c83bb330c0722b18b160be
YoungAndSure <yangshuorex@foxmail.com>
2025-05-18 09:26:05 +0800

    add avg

--- ch01/avg.py
+++ ch01/avg.py
@@ -1,10 +1,22 @@
 #! python
 import numpy as np
 
+data_num = 3
+
 np.random.seed(0)
 rewards = []
-for i in range(11) :
+for i in range(data_num) :
   reward = np.random.rand()
   rewards.append(reward)
-  avg = sum(rewards) / len(rewards)
-  print(reward, avg)
+
+avg = sum(rewards) / len(rewards)
+print(avg)
+
+avg2 = 0.0
+for i in range(data_num) :
+  update_rate = 1 / (i + 1)
+  # NOTE: 
+  avg2 += (rewards[i] - avg2) * update_rate
+print(avg2)
+
+assert(avg == avg2)
