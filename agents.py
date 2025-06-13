db4843c1ee83873aa6d4c05d1747475ea86366bd
YoungAndSure <yangshuorex@foxmail.com>
2025-06-13 17:19:04 +0800

    epsilon algorithm

--- agents.py
+++ agents.py
@@ -36,7 +36,7 @@ class RandomAgent :
       self.cnts[state] += 1
       self.V[state] += (G - self.V[state]) / self.cnts[state]
 
-def greedy_prob(Q, state, action_space) :
+def greedy_prob(Q, state, epsilon=0.0, action_space=[0,1,2,3]) :
   qv = {}
   for action in action_space :
     key = (state, action)
@@ -44,10 +44,16 @@ def greedy_prob(Q, state, action_space) :
 
   max_action = argmax(qv)
 
+  base_prob = epsilon / len(action_space)
+  # 开始有点疑惑，每次都重新搞个新的actions_prob，和固定值有啥区别？这是没理解actions_prob的含义
+  # 这里设置的actions_prob，会影响下一次游戏选择action的概率
+  # 如果设置为0和1，那一旦选到一条好路之后，后面每次都100%选这条路走，其他路就没机会走了，也就得不到其他非本条路的状态的价值
+  # 所以设置的是“概率”，让好的路走的概率高，其他路走的概率低。
+  # eplilon算法的含义就是调整各种选择的概率
   actions_prob = {}
   for action in action_space :
-    actions_prob[action] = 0
-  actions_prob[max_action] = 1
+    actions_prob[action] = base_prob
+  actions_prob[max_action] += 1 - epsilon
 
   return actions_prob
 
@@ -63,6 +69,8 @@ class McAgent() :
     self.Q = defaultdict(lambda : 0)
     self.cnts = defaultdict(lambda : 0)
     self.memory = []
+
+    self.epsilon = 0.0
 
   def get_action(self, state) :
     actions_prob = self.pi[state]
@@ -87,4 +95,4 @@ class McAgent() :
       self.cnts[key] += 1
       self.Q[key] += (G - self.Q[key]) / self.cnts[key]
 
-      self.pi[state] = greedy_prob(self.Q, state, self.action_space)
+      self.pi[state] = greedy_prob(self.Q, state, self.epsilon self.action_space)
